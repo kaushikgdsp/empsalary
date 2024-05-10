018612cf72624c1507dbd8b9ba84ac0f429a747e
kaushik.gdsp <kaushik.gdsp@gmail.com>
2024-05-10 12:43:45 +0530

    Third commit. It is the commit after creating the WAR file

--- empsalary/src/main/java/com/employeemnmt/empsalary/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package com.employeemnmt.empsalary;
-
-public interface EmployeeService  //interface having declarations of all service methods, in order to have loose coupling
-{								  //design pattern
-	public Employee getEmployee();
-	public Employee addEmployee(Employee employee);
-	public double calcBonus(Employee employee);
-	
-}
